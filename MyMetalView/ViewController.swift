9603a499599658920b1b55312dafdcd4a82358ed
Brian Michael Jones <spamthiseml@hotmail.com>
2016-04-30 20:33:59 -0400

    Initial Commit

---
+++ MyMetalView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyMetalView
+//
+//  Created by Brian Jones on 4/30/16.
+//  Copyright © 2016 Brian Jones. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
